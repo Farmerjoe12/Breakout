ef7deb63246f4ff0ec419e67b3d933834a4eee5a
Adam Farmelo <afarmelo@yahoo.com>
2019-10-03 18:52:05 -0400

    Fix assets directory in game.js to /assets

--- breakout.js
+++ breakout.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+var server = require('http').Server(app);
 
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
-app.use('/assets', expres.static(__dirname + '/assets'));
+app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
